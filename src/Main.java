1b93b7351fad90ebd1f440d2fd2a7ae5abe48b51
Raed22xm <71873022+Raed22xm@users.noreply.github.com>
2022-09-26 14:21:43 +0200

    first 26b0n3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("j");
+        System.out.println("mmmmmmmmm");
+    }
+}
